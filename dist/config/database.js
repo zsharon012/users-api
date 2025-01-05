09574ddd5c478ac22ffdbc57a3a0e2e82e27039e
Sharon Zheng <zsharon012@gmail.com>
2025-01-04 18:04:43 -0600

    fix database connection issues

---
+++ dist/config/database.js
@@ -0,0 +1,16 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from "dotenv";
+dotenv.config();
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is required");
+}
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
